e7051ae9d12f600e864154c43049e4324f44cc63
Howard Tang <howard@silk.co>
2015-11-15 13:28:53 +0100

    Tests: Add small pointlist test.

---
+++ tests.js
@@ -0,0 +1,14 @@
+var test   = require("tape");
+var KDTree = require("./index.js");
+
+test("Basic 2d pointlist usecase", function (t) {
+  var points = [[1,2], [3,4], [5,6], [7,8]];
+  var kdt    = KDTree.makeTree(points, 0, 2);
+
+  t.deepEqual(kdt.location, points[2]);
+  t.deepEqual(kdt.left.location, points[1]);
+  t.deepEqual(kdt.left.left.location, points[0]);
+  t.deepEqual(kdt.right.location, points[3]);
+
+  t.end();
+});
