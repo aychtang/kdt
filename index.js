917c756f472d65967f4936afdec36f923026eef1
Howard Tang <howard@silk.co>
2015-11-15 13:20:38 +0100

    KDT: Initial algo outline.

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// <P, N> (loc : P, l : N, r : N) : N
+var make = (loc, l, r) =>
+{
+  return { left     : l
+         , right    : r
+         , location : loc
+         };
+};
+
+// <P, N> ( ls    : Array<P>
+//        , depth : number
+//        , k?    : number )
+//        : N
+var kdtree = function (ls, depth, k)
+{
+  if (!ls.length) return null;
+
+  var axis = depth % (k || 2); // depth mod k (default : 2).
+
+  ls.sort((a, b) => { return a[axis] - b[axis]; });
+
+  var middle = Math.floor(ls.length / 2);
+
+  return make( ls[middle]
+             , kdtree(ls.slice(0, middle)            , depth + 1, k)
+             , kdtree(ls.slice(middle + 1, ls.length), depth + 1, k)
+             );
+};
